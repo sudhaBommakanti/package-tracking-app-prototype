0bca2770fd6c3d32da1db309e4d10736db148207
SudhaBommakanti <mailme.sudha.k@gmail.com>
2020-11-07 19:27:12 +0100

    fetch api using react hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,35 @@
 import './css/App.css';
 import './css/style.css';
 
+import { useState, useEffect } from "react";
+
 import Header from "./components/organisms/Header";
 import Footer from "./components/organisms/Footer";
 
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
+  const endpoint = "https://my.api.mockaroo.com/orders.json?key=e49e6840";
+  const [status, setStatus] = useState(0);
+  const [information, setInformation] = useState([]);
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const response = await fetch(endpoint, { mode: "cors" });
+        const data = await response.json();
+        setInformation(data);
+        setStatus(1);
+      } catch {
+        setStatus(2);
+      }
+    };
+    getData();
+  }, []);
   return (
     <Router>
       <div className="App">
         <Header />
+        {console.log(information)};
         <Switch>
         <Route
             path="/"
